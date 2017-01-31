91fc7fb2fac3e0c39d06f8af9ab2064e791f4160
AyedhSh <alshmary_ad@yahoo.com>
2017-01-31 20:41:26 +0300

    modified the files name

--- lib/hotels_in_iraq.rb
+++ lib/hotels_in_iraq.rb
@@ -1,8 +1,8 @@
 require "hotels_in_iraq/version"
 require_relative "./hotels_in_iraq/city.rb"
-require_relative "./hotels_in_iraq/cline.rb"
+require_relative "./hotels_in_iraq/cli.rb"
 require_relative "./hotels_in_iraq/hotel.rb"
-require_relative "./hotels_in_iraq/scrap.rb"
+require_relative "./hotels_in_iraq/scraper.rb"
 
 require 'colorize'
 require 'open-uri'
